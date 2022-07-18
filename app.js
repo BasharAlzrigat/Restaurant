aa478f7d16cb63bac7f731a2e5394f67ac024262
bashar alzrigat <bashar.zureikat@gmail.com>
2022-07-18 04:36:57 +0300

    commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 'use strict'
 let allMenu = []
-function Menu( name, type, price){
+function Menu(name, type, price) {
     this.foodName = name
     this.type = type
     this.price = price
     allMenu.push(this)
 }
+
 const menuDiv = document.getElementById("Menu")
 const menutable = document.createElement("table")
 menuDiv.appendChild(menutable)
@@ -24,30 +25,33 @@ th2.textContent = "Name"
 th3.textContent = "Type"
 th4.textContent = "Price"
 
-const menuForm = document.getElementById("menu-form");
-menuForm.addEventListener("submit",createNew);
-
-
-function createNew(event){
-    event.preventDefault()
-let name = event.target.foodName.value
-let type = event.target.foodType.value
-let price = event.target.price.value
-let newMenu = new Menu(name, type, price);
-newMenu.render()
-
-}
 new Menu("Pizza", "Fat", "8.35")
 new Menu("Burger", "Fat", "4.25")
 new Menu("Apple", "Fruit", "0.63")
+
+const menuForm = document.getElementById("menu-form");
+menuForm.addEventListener("submit", createNew);
+
+function createNew(event) {
+    event.preventDefault()
+    let name = event.target.foodName.value
+    let type = event.target.foodType.value
+    let price = event.target.price.value
+    let newMenu = new Menu(name, type, price);
+    localStorage.setItem("Menu", JSON.stringify(allMenu))
+    newMenu.render()
+
+}
 Menu.prototype.id = function () {
     let theId = Math.ceil(Math.random() * 10000);
-while (allMenu.includes(theId)){
-    theId = Math.ceil(Math.random() * 10000)
-}
+    while (allMenu.includes(theId)) {
+        theId = Math.ceil(Math.random() * 10000)
+    }
     return theId
 }
-Menu.prototype.render = function(){
+Menu.prototype.render = function () {
+
+
     const tr2 = document.createElement("tr")
     menutable.appendChild(tr2)
     const td1 = document.createElement("td")
@@ -63,13 +67,25 @@ Menu.prototype.render = function(){
     td3.textContent = `${this.type}`
     td4.textContent = `${this.price}`
 
-
 }
 
 function renderAllMenu() {
+    let menuLocalStorage = JSON.parse(localStorage.getItem("Menu"))
+    if (menuLocalStorage !== null) {
+        for (let i = 0; i < menuLocalStorage.length; i++) {
+            new Menu(menuLocalStorage[i].foodName, menuLocalStorage[i].type, menuLocalStorage[i].price);
 
-    for (let i = 0; i < Menu.length; i++) {
-        allMenu[i].render();
+        }
+
     }
+
+
+    for (let i = 0; i < allMenu.length; i++) {
+
+        allMenu[i].render()
+    }
+
 }
+
 renderAllMenu()
+
